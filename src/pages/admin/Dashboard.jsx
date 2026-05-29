3dceae3375a7a3fa9f8518a5086406dd186aa288
nuonghien <hnuong2612dh@gmail.com>
2026-05-29 19:50:20 +0700

    add Dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,9 +1,9 @@
-function Dashboard() {
+export default function Dashboard() {
   return (
     <div>
-      <h1>Admin Dashboard</h1>
+      <h1 className="text-3xl font-bold">
+        Admin Dashboard
+      </h1>
     </div>
-  );
+  )
 }
-
-export default Dashboard;
